79d45ba7c2812be0ea76d62831bb578e6c8d45fc
Sachin Tomar <35167656+sachin0010@users.noreply.github.com>
2026-06-05 17:48:44 +0530

    Update remote_backend.tf

--- terra_practice/remote_backend.tf
+++ terra_practice/remote_backend.tf
@@ -1,6 +1,6 @@
 S3.tf--------.........................
 resource "aws_s3_bucket" "remote_s3" {
-  bucket = "state_bucket"
+  bucket = "skt-state-bucket-2026"
 
   tags = {
     Name        = "My bucket"
@@ -9,28 +9,54 @@ resource "aws_s3_bucket" "remote_s3" {
 }
 
 dynamodb.tf.................................................    through AWS console also we can create
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "skt_table"
-  billing_mode   = "PAY_PER+REQUEST"
-  hash_key       = "LockId"
+resource "aws_dynamodb_table" "basic_dynamodb_table" {
+
+  name         = "skt_table"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
 
   attribute {
     name = "LockID"
-    type = "String"                #or S
+    type = "S"
   }
+
   tags = {
     Name        = "skt_table"
-    Environment = "Dev"                 #or remove
+    Environment = "prod"
   }
 }
 
-.....................................'
+..................................... after initiate the s3 and dynamo then only ater add s3 backend in terr.tf
 terraform init
 terraform validate
 terraform plan
-terraform apply -autu-approve
+terraform apply -auto-approve
 terraform state list
+terraform refresh                   
+
+terraform.tf....................................
+terraform {
+  backend "s3" {
+    bucket         = "skt-state-bucket-2026"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "skt_table"
+    encrypt        = true
+  }
+}
+..............................................
+terraform init
+
+aws s3 ls s3://skt-state-bucket-2026/prod/
+2026-06-05 12:02:43      22368 terraform.tfstate
+
+rm terraform.tfstate
+rm terraform.tfstate.backup
+
+terraform state list    ->  still it will be there using remote backend from s3
 terraform refresh
+
 
 q. if .tfstate deleted then it will restore it from backup and if backeup also deleted
 
