6f92a43fee83cee7e027f91ca6abeb4bbf088675
Sachin Tomar <35167656+sachin0010@users.noreply.github.com>
2026-06-09 13:26:20 +0530

    Update dynamo_db.tf

--- terra_practice/Modules/aws_module_projec/infra_app/dynamo_db.tf
+++ terra_practice/Modules/aws_module_projec/infra_app/dynamo_db.tf
@@ -1,1 +1,20 @@
+resource "aws_dynamodb_table" "basic_dynamodb_table" {
+
+  name         = "${var.my_env}-tws-demo-app-table"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = var.hask_key
+
+  attribute {
+    name = var.hask_key
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.my_env}-tws-demo-app-table"
+    Environment = var.env
+  }
+}
+
+
 
