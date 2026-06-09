efc799a367d7323d2a7728f8214db058268a89fe
Sachin Tomar <35167656+sachin0010@users.noreply.github.com>
2026-06-09 13:21:04 +0530

    Update var.tf

--- terra_practice/Modules/aws_module_projec/infra_app/var.tf
+++ terra_practice/Modules/aws_module_projec/infra_app/var.tf
@@ -2,3 +2,25 @@ variable "my_env" {
   description = "The environment for the app (dev, stg, prd)"
   type        = string
 }
+
+variable "bucket_name" {
+  description = "This is bucket name"
+  type        = string
+}
+
+variable "ec2_ami_id" {
+  type = string
+
+}
+
+variable "instance_count" {
+  description = "this is the no of ec2 instance"
+  type = number
+
+}
+
+variable "instance_type" {
+  description = "this is the inst type"
+  type = string
+
+}
